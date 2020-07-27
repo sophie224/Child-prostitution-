409a6fd5189790b52f74a4164fa7028eae7fd42c
sopiko224 <sophiepkhikidze@gmail.com>
2020-07-27 19:23:10 +0400

    .

--- script.js
+++ script.js
@@ -43,6 +43,3 @@ window.smoothScroll = function (target) {
 var scene = document.getElementById('scene');
 var parallaxInstance = new Parallax(scene);
 
-
-var scene1 = document.getElementById('scene1');
-var parallaxInstance = new Parallax(scene1);
