0ccd80a1c62c7f93284fe91228ef7e7394ebff84
sopiko224 <sophiepkhikidze@gmail.com>
2020-07-30 14:08:14 +0400

    aaa

--- script.js
+++ script.js
@@ -9,7 +9,8 @@ $(window).scroll(function(){
 $(document).ready(function () {
     setTimeout(() => {
         $(".loader-holder").css("display","none");          
-    }, 1500);
+    }, 2000);
+
 });    
 
 
@@ -17,29 +18,29 @@ $('#scroll').click(function(){
     $("html, body").animate({ scrollTop: 0 }, 600); 
     return false; 
 }); 
-window.smoothScroll = function (target) {
-    var scrollContainer = target;
-    do { //find scroll container
-        scrollContainer = scrollContainer.parentNode;
-        if (!scrollContainer) return;
-        scrollContainer.scrollTop += 1;
-    } while (scrollContainer.scrollTop == 0);
+// window.smoothScroll = function (target) {
+//     var scrollContainer = target;
+//     do { //find scroll container
+//         scrollContainer = scrollContainer.parentNode;
+//         if (!scrollContainer) return;
+//         scrollContainer.scrollTop += 1;
+//     } while (scrollContainer.scrollTop == 0);
 
-    var targetY = 0;
-    do { //find the top of target relatively to the container
-        if (target == scrollContainer) break;
-        targetY += target.offsetTop;
-    } while (target = target.offsetParent);
+//     var targetY = 0;
+//     do { //find the top of target relatively to the container
+//         if (target == scrollContainer) break;
+//         targetY += target.offsetTop;
+//     } while (target = target.offsetParent);
 
-    scroll = function (c, a, b, i) {
-        i++; if (i > 30) return;
-        c.scrollTop = a + (b - a) / 30 * i;
-        setTimeout(function () { scroll(c, a, b, i); }, 40);
-    }
-    // start scrolling
-    scroll(scrollContainer, scrollContainer.scrollTop, targetY, 0);
-}
+//     scroll = function (c, a, b, i) {
+//         i++; if (i > 30) return;
+//         c.scrollTop = a + (b - a) / 30 * i;
+//         setTimeout(function () { scroll(c, a, b, i); }, 40);
+//     }
+//     // start scrolling
+//     scroll(scrollContainer, scrollContainer.scrollTop, targetY, 0);
+// }
 
-var scene = document.getElementById('scene');
-var parallaxInstance = new Parallax(scene);
+// var scene = document.getElementById('scene');
+// var parallaxInstance = new Parallax(scene);
 
